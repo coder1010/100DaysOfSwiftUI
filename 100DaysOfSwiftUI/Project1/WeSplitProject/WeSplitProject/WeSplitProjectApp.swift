8f8393e0219c61435242daae85789beba6c4fca4
iChhaya <>
2022-03-17 10:48:54 -0400

    Project1- WeSplit

---
+++ 100DaysOfSwiftUI/Project1/WeSplitProject/WeSplitProject/WeSplitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitProjectApp.swift
+//  WeSplitProject
+//
+//  Created by Chhaya Ahuja on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
