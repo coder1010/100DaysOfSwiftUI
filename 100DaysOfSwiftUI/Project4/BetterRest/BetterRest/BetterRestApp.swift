cfdadd94d275187340fddbc53bcf871c8240cd19
Chhaya Ahuja <ahuja.chhaya@gmail.com>
2022-04-06 19:36:10 -0400

    no message

---
+++ 100DaysOfSwiftUI/Project4/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Chhaya Ahuja on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 100DaysOfSwiftUI/Project5/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Chhaya Ahuja on 4/23/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
